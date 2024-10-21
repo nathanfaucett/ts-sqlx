b1144e99c63d45c40de603405c3da72725b81aed
Nathan Faucett <nathanfaucett@gmail.com>
2024-10-21 05:50:32 -0400

    fix version

--- node/example/.ts-sqlx/src_index.ts.d.ts
+++ node/example/.ts-sqlx/src_index.ts.d.ts
@@ -1,8 +1,6 @@
 import type { SqlxString } from 'ts-sqlx';
 
 declare module 'ts-sqlx' {
-	export function sqlx(query: `select p.* from posts p where p.id = $1;`): SqlxString<[number], {id: number, title: string, content: string, updated_at: Date, created_at: Date}>;
-
 	export function sqlx(query: `select 
 		p.id,
 		p.title,
@@ -10,4 +8,6 @@ declare module 'ts-sqlx' {
 		p.created_at,
 		p.updated_at
 		from posts p`): SqlxString<[], {id: number, title: string, content: string, created_at: Date, updated_at: Date}>;
+
+	export function sqlx(query: `select p.* from posts p where p.id = $1;`): SqlxString<[number], {id: number, title: string, content: string, updated_at: Date, created_at: Date}>;
 }
